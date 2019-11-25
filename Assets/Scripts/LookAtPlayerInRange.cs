daf6e8dff73e566be2741d9995ecb5910156418f
Maurin Thalmann <madotha@hotmail.com>
2019-11-25 09:02:08 +0100

    added tower looking at player in range

---
+++ Assets/Scripts/LookAtPlayerInRange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayerInRange : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float distanceToPlayer = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(target.transform.position, transform.position);
+
+        if (target != null && distance <= distanceToPlayer)
+        {
+            Vector3 lookVector = target.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(lookVector);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+        }
+        
+    }
+}
