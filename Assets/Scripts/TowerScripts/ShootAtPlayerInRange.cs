91e24122481fe4d3dc407fdee6d6ad7e6b4cd08e
Maurin Thalmann <madotha@hotmail.com>
2019-11-25 13:47:03 +0100

    added tower that vomits at player in range
    also restructured project, organization and stuff, skrr

---
+++ Assets/Scripts/TowerScripts/ShootAtPlayerInRange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAtPlayerInRange : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    private float bulletSpeed;
+    [SerializeField]
+    private float distanceToPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(target.transform.position, transform.position);
+
+        if (target != null && distance <= distanceToPlayer)
+        {
+            GameObject bullets = Instantiate(bullet) as GameObject;
+            bullets.transform.position = transform.position;
+            Rigidbody rb = bullets.GetComponent<Rigidbody>();
+            rb.AddForce((target.transform.position - transform.position) * bulletSpeed);
+        }
+
+    }
+}
