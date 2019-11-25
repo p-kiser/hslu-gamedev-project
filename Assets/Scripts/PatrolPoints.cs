2da3d7174677627f312491d504417e6f60e5f694
Maurin Thalmann <madotha@hotmail.com>
2019-11-25 10:56:08 +0100

    added grunt who can follow path of points

---
+++ Assets/Scripts/PatrolPoints.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolPoints : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        GoToNextPoint();
+    }
+
+    void GoToNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 1f)
+            GoToNextPoint();
+    }
+}
