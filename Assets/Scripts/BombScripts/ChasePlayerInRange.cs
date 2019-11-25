a37c1cf0f2f1dbd4840e98540c3347adc7d3962c
Maurin Thalmann <madotha@hotmail.com>
2019-11-25 14:45:51 +0100

    added a bom that chases player in range

---
+++ Assets/Scripts/BombScripts/ChasePlayerInRange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayerInRange : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float distanceToPlayer = 20f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(target.transform.position, transform.position);
+
+        if (target != null && distance <= distanceToPlayer)
+        {
+            agent.destination = target.transform.position;
+        }
+    }
+}
