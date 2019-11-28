3c61b42ad579b8a72d62fd996441221074a06136
Pascal Kiser <pascal.kiser@ajila.com>
2019-11-28 16:50:02 +0100

    added more invisible walls

---
+++ Assets/UiController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour
+{
+    [SerializeField]
+    PlayerController player;
+    [SerializeField]
+    Text scoreText;
+    string s = "Score: ";
+
+
+    int points;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        points = player.GetPoints();
+        scoreText.text = s + points.ToString();
+        Debug.Log(points);
+    }
+}
