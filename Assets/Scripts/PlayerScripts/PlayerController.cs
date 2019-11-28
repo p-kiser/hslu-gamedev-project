3eb6f6053fa4d5fb4cb5281ac20687284838a898
Pascal Kiser <pascal.kiser@ajila.com>
2019-11-28 13:53:53 +0100

    Refactored PlayerController: forces applied to rb are now in FixedUpdate()

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -19,13 +19,12 @@ public class PlayerController : MonoBehaviour
     private float dashDistance = 10.0f;
     [SerializeField]
     private float runningMultiplikator = 3.0f;
-    private bool running = false;
 
     // private variables
     private int jumps = 2;
     private int max_jumps = 2;
     private Vector3 moveDirection = Vector3.zero;
-
+    private bool running;
 
     // Start is called before the first frame update
     void Start()
@@ -38,59 +37,59 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // reset jumps if grounded
+        // Reset jumps if grounded
         if (IsGrounded()) jumps = 0;
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            running = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift)) {
-            running = false;
-        }
-
-        // get movement direction from inputs
+        // Check if we are running
+        running = Input.GetKey(KeyCode.LeftShift) && IsGrounded();
+        
+        // Calculate movement direction from inputs
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         moveDirection = transform.TransformDirection(moveDirection);
-
-
     }
 
     // Everything to do with Rigidbody should be done here
     void FixedUpdate()
     {
         // Jump
-        if (Input.GetButtonDown("Jump") && jumps < max_jumps)
-        {
-            jumps++;
-            Debug.Log("Jumps: " + jumps);
-            //rb.AddForce(Vector3.up * jumpHeight/jumps, ForceMode.Impulse);
-            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight/jumps * -2f * Physics.gravity.y), ForceMode.VelocityChange);
-            anim.Play("Jump");
-        }
+        if (Input.GetKeyDown(KeyCode.Space) && ++jumps < max_jumps) Jump();
 
         // Dash
-        // TODO: Add "dash" in config //if (Input.GetButtonDown("Dash"))
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            Debug.Log("Dash");
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime)));
-            rb.AddForce(dashVelocity, ForceMode.VelocityChange);
-        }
-        // rotate using mouse position mouse
+        if (Input.GetKeyDown(KeyCode.Tab)) Dash();
+
+        // Apply rotation using mouse x value
         transform.Rotate(0, Input.GetAxis("Mouse X") * rotateSpeed, 0);
 
-        // appy movement
+        // Appy movement to player
         moveDirection = moveDirection * speed * (running ? runningMultiplikator : 1);
         rb.MovePosition(rb.position + moveDirection * Time.fixedDeltaTime);
     }
 
 
-    bool IsGrounded()
+    private bool IsGrounded()
     {
         // raycast down to detect ground
         return Physics.Raycast(transform.position, Vector3.down, out _, 1f);
     }
 
+    private void Jump() {
+        Debug.Log("Jumps: " + jumps);
+
+        // advanced mathemagics:
+        float y = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+        rb.AddForce(Vector3.up * y, ForceMode.VelocityChange);
+        anim.Play("Jump");
+    }
+
+    private void Dash() {
+        Debug.Log("Dash");
+
+        // even more advanced mathemagical calculations
+        float x = Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime;
+        float y = 0.0f;
+        float z = Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime;
+        Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3(x, y, z));
+        rb.AddForce(dashVelocity, ForceMode.VelocityChange);
+    }
 
 }
