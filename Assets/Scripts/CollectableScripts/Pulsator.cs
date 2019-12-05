f6d5c6825c384513490d29b70c78f8e98c254e55
Pascal Kiser <pascal.kiser@ajila.com>
2019-12-05 16:29:28 +0100

    added pulsator script

--- Assets/Scripts/CollectableScripts/Pulsator.cs
+++ Assets/Scripts/CollectableScripts/Pulsator.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Pulsator : MonoBehaviour
 {
+    [SerializeField]
+    private float frequency = 2;
+
+    private Vector3 origSize;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        origSize = transform.localScale;
+        Debug.Log(origSize.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 vec = new Vector3(Mathf.Sin(Time.time) + 1, Mathf.Sin(Time.time) + 1, Mathf.Sin(Time.time) + 1);
+        float v = Mathf.Sin(Time.time * frequency);
+        Vector3 vec = new Vector3(v,v,v);
 
-        transform.localScale = vec;
+        transform.localScale = origSize + vec * 0.5f;
     }
 }
