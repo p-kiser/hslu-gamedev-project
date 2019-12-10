6b4839e055552545355e21e01c0f50142542c2d2
Pascal Kiser <pascal.kiser@ajila.com>
2019-12-10 06:51:18 +0100

    vibrator script (wip)

---
+++ Assets/Scripts/CollectableScripts/Vibrator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibrator : MonoBehaviour
+{
+    Vector3 startPosition;
+    [SerializeField]
+    float speed = 100;
+    [SerializeField]
+    float intensity = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float shake = Mathf.Sin(Time.time * speed) * intensity;
+        transform.position = startPosition + new Vector3(shake,shake,shake);
+    }
+}
