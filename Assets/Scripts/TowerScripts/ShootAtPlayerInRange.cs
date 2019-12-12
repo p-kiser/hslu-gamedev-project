61fab17655bd45d74367e62fa5ae19877b7422ed
Pascal Kiser <pascal.kiser@ajila.com>
2019-12-12 19:22:55 +0100

    made some tower updates

--- Assets/Scripts/TowerScripts/ShootAtPlayerInRange.cs
+++ Assets/Scripts/TowerScripts/ShootAtPlayerInRange.cs
@@ -19,9 +19,12 @@ public class ShootAtPlayerInRange : MonoBehaviour
     [SerializeField]
     private int MAX_PROJECTILES = 10;
     [SerializeField]
-    private float INTERVAL = 1.0f;
+    private float RATE_OF_FIRE = 0.25f;
+    [SerializeField]
+    private float INTERVAL = 2.0f;
     private float timer;
     bool wait;
+    bool shooted = false;
     int index;
     
 
@@ -47,43 +50,54 @@ public class ShootAtPlayerInRange : MonoBehaviour
 
         if (target != null && distance <= distanceToPlayer)
         {
+            
             timer += 1.0F * Time.deltaTime;
             if (timer >= INTERVAL) {
                 timer = 0.0f;
                 wait = !wait;
             }
-
-            if (!wait) { Shoot(); }
-            // Reset the velocity and "pause" the physics
-
-
-            /* old legacy code, kept here as a warning to young codeurs:
-             
-            GameObject bullets = Instantiate(bullet) as GameObject;
-            bullets.transform.position = transform.position;
-
-            Rigidbody rb = bullets.GetComponent<Rigidbody>();
-            rb.AddForce((target.transform.position - transform.position) * bulletSpeed);
-            */
             
+
+            if (!wait) {
+                // Reset the velocity and "pause" the physics
+
+                /* old legacy code, kept here as a warning to young codeurs:
+             
+                GameObject bullets = Instantiate(bullet) as GameObject;
+                bullets.transform.position = transform.position;
+
+                Rigidbody rb = bullets.GetComponent<Rigidbody>();
+                rb.AddForce((target.transform.position - transform.position) * bulletSpeed);
+                */
+
+                if (!shooted) {
+                    Shoot(index);
+                    index = ++index % MAX_PROJECTILES;
+                    //shooted = true;
+                    Invoke("ToggleShooted", RATE_OF_FIRE);
+                } 
+                
+            }
         }
     }
 
-    void Shoot() {
-        rbs[index].velocity = Vector3.zero;
-        rbs[index].angularVelocity = Vector3.zero;
-        rbs[index].isKinematic = true;
+    void Shoot(int i) {
+        rbs[i].velocity = Vector3.zero;
+        rbs[i].angularVelocity = Vector3.zero;
+        rbs[i].isKinematic = true;
         // Do positioning, etc
-        projectiles[index].transform.position = rbs[index].position;
-        projectiles[index].transform.rotation = Quaternion.identity;
+        projectiles[i].transform.position = rbs[i].position;
+        projectiles[i].transform.rotation = Quaternion.identity;
         // Re-enable the physics and set start position to that of the turret
-        rbs[index].isKinematic = false;
-        projectiles[index].transform.position = transform.position;
+        rbs[i].isKinematic = false;
+        projectiles[i].transform.position = transform.position;
+        projectiles[i].SetActive(true);
 
         // shoot projectile and increase the counter
         rbs[index].AddForce((target.transform.position - transform.position) * bulletSpeed);
 
         Debug.Log("Shooteded a projectile called: " + index);
-        index = ++index % MAX_PROJECTILES;
     }
+
+    void ToggleShooted() { shooted = !shooted; }
 }
