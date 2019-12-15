9db0052817e28c7f9ebc6d0295f6e3e486fad4b7
Maurin Thalmann <madotha@hotmail.com>
2019-12-15 15:21:39 +0100

    created first killable tower

---
+++ Assets/Scripts/KillEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillEnemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private float bounceUpForce;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("DAMN you killed that enemy hard!");
+            GameObject.Destroy(gameObject);
+            other.attachedRigidbody.AddForce(Vector3.up * bounceUpForce, ForceMode.Impulse);
+        }
+        
+        
+    }
+}
