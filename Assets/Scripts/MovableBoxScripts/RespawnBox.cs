c612a219b3bac49a94b04205a7f8fefdcaa42a09
SaschaSaegesser <32762408+SaschaSaegesser@users.noreply.github.com>
2019-12-15 17:29:37 +0100

    MovableBox respawns now when dropped into killzone

---
+++ Assets/Scripts/MovableBoxScripts/RespawnBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnBox : MonoBehaviour
+{
+    private Vector3 respawnPosition;
+    private Quaternion respawnRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPosition = transform.position;
+        respawnRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Deathzone"))
+        {
+            Debug.Log("MoveableBox respawned");
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        transform.position = respawnPosition;
+        transform.rotation = respawnRotation;
+    }
+}
