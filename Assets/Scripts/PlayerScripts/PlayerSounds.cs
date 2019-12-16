0934c168e171343937f4d149bf851bc32dc8842b
Maurin Thalmann <madotha@hotmail.com>
2019-12-16 14:20:05 +0100

    sound feedback when player is damaged

---
+++ Assets/Scripts/PlayerScripts/PlayerSounds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip damageSound;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy") ||
+            other.gameObject.CompareTag("EnemyBig"))
+        {
+            AudioSource.PlayClipAtPoint(damageSound, gameObject.transform.position);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            AudioSource.PlayClipAtPoint(damageSound, gameObject.transform.position);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") || 
+            collision.gameObject.CompareTag("EnemyBig"))
+        {
+            AudioSource.PlayClipAtPoint(damageSound, gameObject.transform.position);
+        }
+
+    }
+}
