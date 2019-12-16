68ee5316ab5a3c0b63dc3d16e1d3dadad6544300
Pascal Kiser <pascal.kiser@ajila.com>
2019-12-16 21:28:29 +0100

    some stuff

---
+++ Assets/PlaySoundOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip clip;
+
+    private void OnCollisionEnter(Collider other)
+    {
+        Debug.Log("Collision");
+        AudioSource.PlayClipAtPoint(clip, gameObject.transform.position);
+
+    }
+}
