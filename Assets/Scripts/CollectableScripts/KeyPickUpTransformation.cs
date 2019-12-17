ed3ca5ba95a08ae26bd937b42a5c1f0222678c3e
SaschaSaegesser <32762408+SaschaSaegesser@users.noreply.github.com>
2019-12-17 14:36:44 +0100

    Key can only be picked up by player now (turret stole my key)

--- Assets/Scripts/CollectableScripts/KeyPickUpTransformation.cs
+++ Assets/Scripts/CollectableScripts/KeyPickUpTransformation.cs
@@ -15,16 +15,15 @@ public class KeyPickUpTransformation : MonoBehaviour
         // Teleport the player back to the hub
         if (other.GetComponentInParent<Rigidbody>().tag == "Player" && gameObject.transform.tag != "CollectedKey")
         {
+            // Add the key to the corresponding key pedestal
+            gameObject.transform.position = keyPedestal.position + new Vector3(0.0f, 2.0f, 0.0f);
+            gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            gameObject.transform.tag = "CollectedKey";
+
             Transform playerTransform = other.GetComponentInParent<Rigidbody>().transform;
             playerTransform.position = returnPosition.position;
             playerTransform.rotation = returnPosition.rotation;
         }
-
-        // Add the key to the corresponding key pedestal
-        gameObject.transform.position = keyPedestal.position + new Vector3(0.0f, 2.0f, 0.0f);
-        gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-        gameObject.transform.tag = "CollectedKey";
-
     }
 
     // Start is called before the first frame update
